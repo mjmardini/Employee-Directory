3d0b5d482e1d29e282066cc7220aa05051f61dbb
mjm26 <m.mmaster@hotmail.com>
2018-02-09 00:53:41 +0200

    Minor Changes to css

--- src/App.js
+++ src/App.js
@@ -26,6 +26,9 @@ class App extends Component {
                 <EmployeeCard/>
                 <EmployeeCard/>
                 <EmployeeCard/>
+                <EmployeeCard/>
+                <EmployeeCard/>
+                <EmployeeCard/>
             </div>
         );
     }
